484975e3494ed19d4c750967638800c2f5df743c
Elyor Djalilov <edjalilov.dev@gmail.com>
2026-02-07 17:23:54 +0500

    chore: update dependencies, and remove unused components

--- src/app/layouts/main-layout/ui/main-layout.tsx
+++ src/app/layouts/main-layout/ui/main-layout.tsx
@@ -1,7 +1,6 @@
 import cn from './main-layout.module.css'
 
 import { AppShell } from '@mantine/core'
-import classNames from 'classnames'
 import { Suspense } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { Outlet } from 'react-router-dom'
@@ -9,7 +8,7 @@ import { Outlet } from 'react-router-dom'
 export const MainLayout = () => {
   return (
     <AppShell>
-      <AppShell.Main className={classNames(cn.main)}>
+      <AppShell.Main className={cn.main}>
         <ErrorBoundary fallback={'Error...'}>
           <Suspense>
             <Outlet />
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,3 +1,2 @@
-export { BaseModal } from './base-modal/base-modal'
 export { ErrorMessage } from './error-message/error-message'
 export { Loader } from './loader/loader'
